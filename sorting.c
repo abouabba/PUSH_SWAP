a33b653d618370bc0a7ce6ad5d2c26a82f293b0f
Abdessamad Bouabbadi <abouabba@e1r4p8.1337.ma>
2025-01-27 23:04:38 +0100

    push

--- sorting.c
+++ sorting.c
@@ -12,21 +12,44 @@
 
 #include "push_swap.h"
 
-int sort_3(t_stack **stack_a)
+int	sort_3(t_stack **stack_a)
 {
 	t_stack	*first;
 	t_stack	*second;
 	t_stack	*third;
 
-	first = (*stack_a)->value;
-	second = (*stack_a)->next->value;
-	third = (*stack_a)->next->next->value;
-	
-    if (first < second && second < third)
-        return (0);
-	else if(first < second && third < second)
-		{
-			
-		}
+	first = *stack_a;
+	second = (*stack_a)->next;
+	third = (*stack_a)->next->next;
+
+	if (first->value < second->value && second->value < third->value)
+		return (0);
+	if (first->value < third->value && third->value < second->value)
+	{
+		sa(stack_a);
+		return (0);
+	}
+	if (second->value < first->value && first->value < third->value)
+	{
+		sa(stack_a);
+		ra(stack_a);
+		return (0);
+	}
+	if (second->value < third->value && third->value < first->value)
+	{
+		rra(stack_a);
+		return (0);
+	}
+	if (third->value < first->value && first->value < second->value)
+	{
+		ra(stack_a);
+		return (0);
+	}
+	if (first->value > second->value && second->value > third->value)
+	{
+		sa(stack_a);
+		rra(stack_a);
+		return (0);
+	}
 	return (-1);
 }
