077ab8b79ce743a3a1322246404f68ad2f52da13
Abdessamad Bouabbadi <abouabba@e1r12p4.1337.ma>
2025-01-30 21:20:33 +0100

    push

--- push_swap.h
+++ push_swap.h
@@ -28,10 +28,24 @@ void		stack_clear(t_stack **head);
 t_stack		*new_stack(int n);
 int			push_back(t_stack **head, int n);
 int			stack_size(t_stack *s);
+int	sorting_3(t_stack **stack_a);
 
 void		swap(t_stack **stack);
 void		push(t_stack **dst, t_stack **src);
 void		rotate(t_stack **stack);
 void		reverse_rotate(t_stack **stack);
+void	add_front(t_stack **head, t_stack *new);
+void	push(t_stack **dst, t_stack **src);
+void	pb(t_stack **stack_a, t_stack **stack_b);
+void	pa(t_stack **stack_a, t_stack **stack_b);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack ** stack_b);
+void	rrr(t_stack ** stack_a, t_stack **stack_b);
+void	ra(t_stack **stack_a);
+void	rb(t_stack **stack_b);
+void	rr(t_stack **stack_a, t_stack **stack_b);
+void	sa(t_stack **stack_a);
+void	sb(t_stack **stack_b);
+void	ss(t_stack **stack_a, t_stack **stack_b);
 
 #endif
