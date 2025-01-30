f972c52ce503ecfd850e98b66b324d3d498457f7
bouabbadi abdessamad <bouabbadiabdessamad@gmail.com>
2025-01-30 19:59:02 +0100

    Update sorting_3.c

--- sorting_3.c
+++ sorting_3.c
@@ -12,44 +12,46 @@
 
 #include "push_swap.h"
 
-int	sorting_3(t_stack **stack_a)
+int sorting_3(t_stack **stack_a)
 {
-	t_stack	*first;
-	t_stack	*second;
-	t_stack	*third;
+    t_stack	*first;
+    t_stack	*second;
+    t_stack	*third;
 
-	first = *stack_a;
-	second = (*stack_a)->next;
-	third = (*stack_a)->next->next;
+    if (!stack_a || !(*stack_a) || !(*stack_a)->next || !(*stack_a)->next->next)
+        return (-1);
+
+    first = *stack_a;
+    second = (*stack_a)->next;
+    third = (*stack_a)->next->next;
 
 	if (first->value < second->value && second->value < third->value)
-		return (0);
-	if (first->value < third->value && third->value < second->value)
-	{
-		sa(stack_a);
-		return (0);
-	}
-	if (second->value < first->value && first->value < third->value)
-	{
-		sa(stack_a);
-		ra(stack_a);
-		return (0);
-	}
-	if (second->value < third->value && third->value < first->value)
-	{
-		rra(stack_a);
-		return (0);
-	}
-	if (third->value < first->value && first->value < second->value)
-	{
-		ra(stack_a);
-		return (0);
-	}
-	if (first->value > second->value && second->value > third->value)
-	{
-		sa(stack_a);
-		rra(stack_a);
-		return (0);
-	}
-	return (-1);
+		return(0);
+    if (first->value < third->value && third->value < second->value)
+    {
+	rra(stack_a);
+        sa(stack_a);
+        return (0);
+    }
+    if (second->value < first->value && first->value < third->value)
+    {
+        sa(stack_a);
+        return (0);
+    }
+    if (second->value < third->value && third->value < first->value)
+    {
+        ra(stack_a);
+        return (0);
+    }
+    if (third->value < first->value && first->value < second->value)
+    {
+        rra(stack_a);
+        return (0);
+    }
+    if (first->value > second->value && second->value > third->value)
+    {
+        sa(stack_a);
+        return (0);
+    }
+    return (-1);
 }
